731287c16e36303f51614e6d38316c927d278910
Austin Bower <bowera18@mycu.concord.edu>
2016-04-01 09:42:02 -0400

    Minor visual adjustments, edit and take att. added
    Made some slight visual alterations and added the changes/work that Matt
    has done to the edit and take attendance screens

---
+++ StudentLogin.cs
@@ -0,0 +1,96 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using System;
+using MySql.Data.MySqlClient;
+/*This class accesses the database and checks to see if the information swiped in is a current student
+in the class. If the swipe is valid, the date and time are inputted into the database.
+Comment updated 3/25/2016.*/
+namespace StudentAttendanceTracker
+{
+
+    public class StudentLogin
+    {
+
+        private MySqlConnection connectionVariable;
+        private MySqlCommand searchCommand;
+        private MySqlCommand insertCommand;
+        private bool studentFound = false;
+        private string userInput;
+        private string studentIdSubstring;
+        private string dateSubstring;
+        // Takes studentId information from string and checks it against known IDs in the database. Comment updated 3/25/2016.
+        public void databaseLoginCheck()
+        {
+
+            studentIdSubstring = userInput.Substring(0, 9);
+            connectionVariable = new MySqlConnection();
+            connectionVariable.ConnectionString = "server=127.0.0.1;uid=root;pwd=;database=swipecard;";
+            connectionVariable.Open();
+            searchCommand = new MySqlCommand("SELECT * FROM student WHERE StudentNumber = " + studentIdSubstring, connectionVariable);
+            MySqlDataReader reader = searchCommand.ExecuteReader();
+            if(reader.HasRows)
+            {
+
+                connectionVariable.Close();
+                studentFound = true;
+
+            }// if(reader.HasRows)
+            insertTime(studentFound, connectionVariable);
+            connectionVariable.Close();
+
+        }// public bool databaseLoginCheck()
+        // Method that inserts the time into the date column from the user input. Comment updated 3/29/2016.
+        private void insertTime(bool foundInput, MySqlConnection connectionInput)
+        {
+
+            if(foundInput)
+            {
+                dateSubstring = userInput.Substring(10);
+                string findQuery = "SELECT Date FROM student WHERE StudentNumber =" + userInput.Substring(0, 9) + ";";
+                searchCommand = new MySqlCommand(findQuery, connectionInput);
+                connectionInput.Open();
+                MySqlDataReader reader;
+                reader = searchCommand.ExecuteReader();
+                reader.Read();
+                string currentDateString = reader.GetString("Date");
+                currentDateString += "\n" + dateSubstring;
+                reader.Close();
+
+                string query = "UPDATE swipecard.student SET Date ='" + currentDateString + "' WHERE StudentNumber = " + userInput.Substring(0, 9) + ";";
+                insertCommand = new MySqlCommand(query, connectionInput);
+                reader = insertCommand.ExecuteReader();
+                while(reader.Read())
+                {
+
+                }// while(reader.Read())
+
+            }// if(foundInput) 
+
+        }// private static void insertTime(bool foundInput)
+        // Method fills the dateSubstring and timeSubstring. Comment updated 3/29/2016.
+        public bool StudentFound
+        {
+
+            get
+            {
+
+                return studentFound;
+
+            }// get
+
+        }// public bool StudentFound
+        public string UserInput
+        {
+
+            set
+            {
+
+                userInput = value;
+
+            }// set
+
+        }// public string UserInput
+
+    }// public class StudentLogin
+
+}// namespace Example
---
+++ loginScreen.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace StudentAttendanceTracker
+{
+    /// <summary>
+    /// Interaction logic for loginScreen.xaml
+    /// </summary>
+    public partial class loginScreen : Window
+    {
+        public loginScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        //check the credentials entered in the text boxes, advance to home screen if valid. else tell user credentials are invalid
+        private void loginSubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            * Check the validity of the credentials against the database here!!!
+            */
+            string username = this.usernameTextBox.Text;
+            string password = this.loginPasswordBox.Password;
+            //if username & password are invalid
+            MySqlConnection connectionVariable;
+            MySqlCommand searchCommand;
+            connectionVariable = new MySqlConnection();
+            connectionVariable.ConnectionString = "server=localhost;uid=root;pwd=;database=professor;";
+            searchCommand = new MySqlCommand("SELECT * FROM professorinformation WHERE username ='" + username +"'", connectionVariable);
+            connectionVariable.Open();
+            MySqlDataReader reader = searchCommand.ExecuteReader();
+            if(reader.HasRows)
+            {
+                homeScreen home = new homeScreen();
+                home.Show();
+                this.Close();
+                connectionVariable.Close();
+            }
+            //if username & password are valid...
+            else
+            {
+                MessageBox.Show("INVALID CRENDENTIALS! The username / password combination entered is invalid, please try again.", "Invalid Credentials");
+            }
+        }
+
+        //close this login window and open up the welcome window
+        private void loginGoBackButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow welcome = new MainWindow();
+            welcome.Show();
+            this.Close();
+        }
+    }
+}
