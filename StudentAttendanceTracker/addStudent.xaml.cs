359f263ce2a5bb2b52683ec786709fc86d0e4060
smithh05 <smithh05@mycu.concord.edu>
2016-04-06 14:20:40 -0400

    Add Student
    Add Student Linked to Database

--- StudentAttendanceTracker/addStudent.xaml.cs
+++ StudentAttendanceTracker/addStudent.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace StudentAttendanceTracker
 {
@@ -29,6 +30,47 @@ namespace StudentAttendanceTracker
             homeScreen homeScreenWindow = new homeScreen();
             homeScreenWindow.Show();
             this.Close();
+        }
+
+        private void addStudentSubmit_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            MessageBox.Show("Initiate");
+            MySqlConnection conn;
+            MySqlCommand addStudentCommand;
+            conn = new MySqlConnection();
+            conn.ConnectionString = "server=127.0.0.1;uid=root;pwd=;database=studentinfo;";
+
+            MySqlCommand checkStudentID = new MySqlCommand("SELECT COUNT(*) FROM studentinformation WHERE StudentNumber = '" + studentID.Text + "'", conn);
+            try
+            {
+                conn.Open();
+                int totalCount = Convert.ToInt32(checkStudentID.ExecuteScalar());
+                string studentIDValue = studentID.Text;
+                if (totalCount == 0 && studentIDValue.Length == 9)
+                {
+                    addStudentCommand = new MySqlCommand("INSERT INTO studentinformation (StudentNumber, StudentName) VALUES('" + studentID.Text + "','" + studentName.Text + "')", conn);
+                    //conn.Open();
+                    addStudentCommand.ExecuteNonQuery();
+                    MessageBox.Show("Success");
+                    conn.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Student ID already exists!");
+                    conn.Close();
+                }
+            }
+            catch(MySqlException ex)
+            {
+                MessageBox.Show("Exception");
+            }
+
+            /*addStudentCommand = new MySqlCommand("INSERT INTO studentinformation (StudentNumber, StudentName) VALUES('" + studentID.Text + "','" + studentName.Text + "')", conn);
+            conn.Open();
+            addStudentCommand.ExecuteNonQuery();
+            MessageBox.Show("Success");
+            conn.Close();*/
         }
     }
 }
