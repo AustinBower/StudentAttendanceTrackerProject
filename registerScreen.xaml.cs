c99114fd47f14578218de57df57c24de3c829b1f
minnixz19 <virtuosoikingpin@gmail.com>
2016-04-06 13:20:23 -0400

    Error Checking
    Added error checking for all fields.

--- registerScreen.xaml.cs
+++ registerScreen.xaml.cs
@@ -28,6 +28,10 @@ namespace StudentAttendanceTracker
 
         private void registerSubmitButton_Click(object sender, RoutedEventArgs e)
         {
+            formFilled.Content = "";
+            passwordNotEqual.Content = "";
+
+            //puts everything in textbox's into strings
             String username = usernameTextBox.Text;
             String firstname = firstnameTextBox.Text;
             String lastname = lastnameTextBox.Text;
@@ -35,9 +39,22 @@ namespace StudentAttendanceTracker
             String password = passwordTextBox.Text;
             String confirmPassword = confirmpasswordTextBox.Text;
 
-            if (password == confirmPassword)
+            //checks to see if all of the fields at least have something, and checks to see if passwords are equal
+           
+
+            if (password == confirmPassword && username.Length > 0 && firstname.Length > 0 && lastname.Length > 0 && email.Length > 0 && password.Length > 0)
             {
 
+            }
+
+            else if (username.Length == 0 || firstname.Length == 0 || lastname.Length == 0 || email.Length == 0 || password.Length == 0)
+            {
+                formFilled.Content = "Please complete every field.";
+            }
+
+            else if (password != confirmPassword)
+            {
+                passwordNotEqual.Content = "Your passwords do not match!";
             }
 
         }
