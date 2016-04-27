64043e75958e288d05fd903d0efa12b09d21f360
YIngjunLiao <yingjun.liao@gmail.com>
2016-04-27 09:45:53 -0400

    New Version of Project
    Yingjun submitted

---
+++ Version 2/loginScreen.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace StudentAttendanceTracker
+{
+    
+    /// Interaction logic for loginScreen.xaml
+    public partial class loginScreen : Window
+    {
+        public loginScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        //check the credentials entered in the text boxes, advance to home screen if valid. else tell user credentials are invalid
+        private void loginSubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            * Check the validity of the credentials against the database here!!!
+            */
+            string username = this.usernameTextBox.Text;
+            string password = this.loginPasswordBox.Password;
+            //if username & password are invalid
+            
+            SqlConnection connectionVariable = new SqlConnection(@"Data Source=YINGJUN\SQLEXPRESS;Initial Catalog=StudentAttendanceTracker;Integrated Security=True");
+            SqlDataAdapter searchCommand = new SqlDataAdapter("SELECT Count(*) FROM ProfessorInfo WHERE UserName ='" + username +"' and Password = '" + password + "'", connectionVariable);
+            connectionVariable.Open();
+            DataTable dt = new DataTable();
+            searchCommand.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                homeScreen home = new homeScreen();
+                home.Show();
+                this.Close();
+            }
+            //if username & password are valid...
+            else
+            {
+                passwordNotEqual.Content = "The Username and Password combination entered is invalid, please try again!";
+            }
+        }
+
+        //close this login window and open up the welcome window
+        private void loginGoBackButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow welcome = new MainWindow();
+            welcome.Show();
+            this.Close();
+        }
+    }
+}
