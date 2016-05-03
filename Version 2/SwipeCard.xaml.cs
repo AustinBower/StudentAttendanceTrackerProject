c5f5ebf3305be2cecaa19dc8d4e57c20e67ba144
gamecreator94 <rawsonmb@gmail.com>
2016-05-03 12:47:35 -0400

    Comments for Student Swipe

--- Version 2/SwipeCard.xaml.cs
+++ Version 2/SwipeCard.xaml.cs
@@ -19,7 +19,7 @@ using System.Windows.Threading;
 namespace StudentAttendanceTracker
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for SwipeCard.xaml
     /// </summary>
     public partial class SwipeCard : Window
     {
@@ -40,33 +40,49 @@ namespace StudentAttendanceTracker
         private string studentIdSubstring;
         private string classNameInput;
         private string courseNumberString;
-        //private int courseNumberInput;
         bool justSwiped;
         static SwipeCard()
         {
+
             timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromSeconds(3);
+
         }// static StudentSwipe()
+        /// <summary>
+        /// Method that intializes the events for the StudentSwipe UI. Comment updated 4/20/2016.
+        /// </summary>
         public SwipeCard()
         {
+
             InitializeComponent();
             justSwiped = false;
             timer.Tick += timeDelay;
             successTimer = new System.Timers.Timer(3000);
             student_instruction_block.Visibility = Visibility.Visible;
             student_ID_input_box.Focus();
-        }
 
+        }
+        /// <summary>
+        /// Button event used to return to the menu screen. Comment updated 4/20/2016.
+        /// </summary>
+        /// <param name="sender"> Default parameter for the UI event. Comment updated 4/20/2016. </param>
+        /// <param name="e"> Default parameter for the UI event. Comment updated 4/20/2016. </param>
         private void go_back_button_Click(object sender, RoutedEventArgs e)
         {
+
             homeScreen homeScreenWindow = new homeScreen();
             homeScreenWindow.Show();
             this.Close();
-        }
 
+        }
+        /// <summary>
+        /// Event for textbox used to input ID swipe. Comment updated 4/20/2016.
+        /// </summary>
+        /// <param name="sender"> Default parameter for the UI event. Comment updated 4/20/2016. </param>
+        /// <param name="e"> Default parameter for the UI event. Comment updated 4/20/2016. </param>
         private void student_ID_input_box_TextChanged(object sender, TextChangedEventArgs e)
         {
-            // Student ID is read in here.
+
             TextBox textInfomationVariable = sender as TextBox;
             studentInputVariable = textInfomationVariable.Text;
             if (studentInputVariable.Length == 18 && !justSwiped)
@@ -98,37 +114,45 @@ namespace StudentAttendanceTracker
             }
 
         }
-       
+        /// <summary>
+        /// Returns date the user swiped in. Comment updated 3/25/2016.
+        /// </summary>
+        /// <param name="Date"> Brings in the information from the class DateTime. Comment updated 4/202/2016. </param>
+        /// <returns> Returns the date the user swiped in. Comment updated 4/20/2016. </returns>
         public string GetDate(DateTime Date)
         {            
+
             string tempStringVariable = "";
             string[] DateStringVariable = Date.GetDateTimeFormats(); 
             tempStringVariable = DateStringVariable[6];
             tempStringVariable=tempStringVariable.Replace("/", "-");
             return tempStringVariable;
+
         }
-       
+        /// <summary>
+        /// Takes studentId information from string and checks it against known IDs in the database. Comment updated 3/25/2016.
+        /// </summary>
         public void databaseLoginCheck()
         {
 
             studentFound = false;
-            studentIdSubstring = userInput.Substring(0, 9);
-            /*
-            *Database connection using SQL Server
-            */           
+            studentIdSubstring = userInput.Substring(0, 9);         
             connectionVariable = new SqlConnection(@"Data Source=YINGJUN\SQLEXPRESS;Initial Catalog=StudentAttendanceTracker;Integrated Security=True");
             connectionVariable.Open();
             searchCommand = new SqlCommand("SELECT * FROM StudentInfo WHERE StudentNumber = " + studentIdSubstring, connectionVariable);
             SqlDataReader reader = searchCommand.ExecuteReader();
             if (reader.HasRows)
             {
+
                 studentFound = true;
 
             }// if(reader.HasRows
             connectionVariable.Close();
 
         }
-
+        /// <summary>
+        /// Method that inserts the time into the date column from the user input. Comment updated 3/29/2016. 
+        /// </summary>
         private void insertTime(bool foundInput, SqlConnection connectionInput)
         {
             connectionVariable = new SqlConnection(@"Data Source=YINGJUN\SQLEXPRESS;Initial Catalog=StudentAttendanceTracker;Integrated Security=True");
@@ -153,6 +177,12 @@ namespace StudentAttendanceTracker
             connectionInput.Close();
 
         }
+        /// <summary>
+        /// Method that creates a moment where the software does not make any changes until amount of
+        /// time has passed. Comment updated 3/25/2016.
+        /// </summary>
+        /// <param name="sender"> Default parameter for the UI event. Comment updated 4/20/2016. </param>
+        /// <param name="e"> Default parameter for the UI event. Comment updated 4/20/2016. </param>
         private void timeDelay(object sender, EventArgs e)
         {
 
@@ -169,7 +199,12 @@ namespace StudentAttendanceTracker
             student_ID_input_box.Focus();
 
         }
-
+        /// <summary>
+        /// Adds 1 to the attendance after each swipe to show how many swipes have been made by the student.
+        /// Comment updated 4/16/2016.
+        /// </summary>
+        /// <param name="userId"> UserId brings in the student ID number to be used in the database
+        /// for selecting the student who swiped in. Comment updated 4/20/2016. </param>
         private void studentAttendanceTracker(string userId)
         {
 
@@ -180,7 +215,8 @@ namespace StudentAttendanceTracker
             connectionVariable.Open();
             updateCommand.ExecuteNonQuery();
             connectionVariable.Close();
-       }
+
+        }
 
         /*public void retrieveCourseNumber(string userInput)
         {
@@ -195,7 +231,11 @@ namespace StudentAttendanceTracker
             connectionVariable.Close();
             
          }*/
-
+        /// <summary>
+        /// Combobox event that allows the professors to select which class the student swipes for. Comment updated 4/20/2016.
+        /// </summary>
+        /// <param name="sender"> Default parameter for the UI event. Comment updated 4/20/2016. </param>
+        /// <param name="e"> Default parameter for the UI event. Comment updated 4/20/2016. </param>
         private void class_select_comboBox_Initialized(object sender, EventArgs e)
         {
 
@@ -216,4 +256,5 @@ namespace StudentAttendanceTracker
         }
 
     }
+
 }
